64cee31a3e40e72a86c48fd301f45fada08825bb
Bernhard Why <hello@wytek.info>
2024-11-14 15:05:52 +0100

    add tests

--- cypress/e2e/basics.cy.ts
+++ cypress/e2e/basics.cy.ts
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
 describe('programs', () => {
-  it('should render main image', () => {
-    cy.visit('http://localhost:4200/programs')
+  it('should render program list', () => {
+    cy.visit('http://localhost:4200/programs');
+    cy.get('[data-test="program-list"]');
+  })
+
+  it('should display the page title', () => {
+    cy.visit('http://localhost:4200/programs');
+    cy.get('app-header').contains('Home');
+  })
+
+  it('should display a program item', () => {
+    cy.visit('http://localhost:4200/programs');
+    cy.get('[data-test="program-item"]');
   })
 })
