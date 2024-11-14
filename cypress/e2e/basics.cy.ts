ba439da47fb1c309eee247b645d3d5c37cebe2bf
Bernhard Why <hello@wytek.info>
2024-11-14 15:37:50 +0100

    new tests to check for backend

--- cypress/e2e/basics.cy.ts
+++ cypress/e2e/basics.cy.ts
@@ -6,13 +6,8 @@ describe('programs', () => {
     cy.get('[data-test="program-list"]');
   })
 
-  it('should display the page title', () => {
-    cy.visit('http://localhost:4200/programs');
-    cy.get('app-header').contains('Home');
-  })
-
-  it('should display a program item', () => {
-    cy.visit('http://localhost:4200/programs');
-    cy.get('[data-test="program-item"]');
+  it('should display the correct week title', () => {
+    cy.visit('http://localhost:4200/programs/234015');
+    cy.get('[data-test="week-title"]').contains('Week 1');
   })
 })
